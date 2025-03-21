31e0b610954bfc1013fd7d99cca7e229eb92a33b
Okoko <osa37@uclive.ac.nz>
2025-03-22 12:11:23 +1300

    Working with tauri API

--- badminton-tool/README.md
+++ badminton-tool/README.md
@@ -34,3 +34,13 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+
+## Run this
+npm install @tauri-apps/api@latest @tauri-apps/cli@latest
+npm list @tauri-apps/api @tauri-apps/cli
+
+- output should be:
+    @tauri-apps/api@2.4.0
+    └── @tauri-apps/cli@2.4.0
+
--- badminton-tool/src/app/pages/greet.tsx
+++ badminton-tool/src/app/pages/greet.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from '@tauri-apps/api/core';
+import { useEffect, useState } from 'react';
 
 export default function Greet() {
-  const [greeting, setGreeting] = useState("");
-  useEffect(() => {
-    if (window && "__TAURI_IPC__" in window) {
-      // Only call Tauri APIs if running in a Tauri environment
-      invoke<string>("greet", { name: "Next.js" })
-        .then((result) => setGreeting(result))
-        .catch(console.error);
-    } else {
-      console.warn("Not running in a Tauri environment");
-      setGreeting("Hello from a non-Tauri environment!");
-    }
-  }, []);
+  const [greeting, setGreeting] = useState('');
 
+  useEffect(() => {
+    invoke<string>('greet', { name: 'Next.js' })
+      .then(result => setGreeting(result))
+      .catch(console.error)
+  }, [])
+
+  // Necessary because we will have to use Greet as a component later.
   return <div>{greeting}</div>;
 }
